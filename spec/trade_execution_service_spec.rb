0c4d984c7fd40a2093002118196e5ebec51c66ca
ignat99 <ignat99@spain.holding.bz>
2018-12-09 01:06:29 +0200

    JSON

---
+++ spec/trade_execution_service_spec.rb
@@ -0,0 +1,15 @@
+require "rspec"
+require "httparty"
+require "json"
+require_relative "../app/order"
+require_relative "../trade_execution_service"
+
+describe TradeExecutionService do
+
+  before(:each) do
+    @order = Order.new :side => "a", :size => 100, :currency => "USD", :counter_currency => 5, :date => "8/10/2018", :price => 200, :order_id => 150, :lp => "lpA"
+    @tes = TradeExecutionService.new
+  end
+
+
+end
